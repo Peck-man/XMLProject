42a41fd2e5ea1813ccc52ea167c03750a1f9ce74
Peck-man <simonpeck97@gmail.com>
2022-09-27 17:10:37 +0200

    village repository

--- src/main/java/com/example/xmlproject/models/Village.java
+++ src/main/java/com/example/xmlproject/models/Village.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Village {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Integer id;
     private String name;
 
     public Village(String name) {
---
+++ src/main/java/com/example/xmlproject/repositories/VillageRepository.java
@@ -0,0 +1,7 @@
+package com.example.xmlproject.repositories;
+
+import com.example.xmlproject.models.Village;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VillageRepository extends JpaRepository<Village, Integer> {
+}
